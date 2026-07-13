396a2a2f6f7aeb90aece3bcd68cffc65ce8ef328
zoltys <capdomicile@gmail.com>
2026-07-13 21:05:54 +0200

    installation projet veille ehpad

---
+++ france_travail.py
@@ -0,0 +1,31 @@
+{
+  "location": {
+    "commune": "Le Castellet",
+    "postal_code": "83330",
+    "latitude": 43.2025,
+    "longitude": 5.7769,
+    "radius_km": 50
+  },
+  "contracts": [
+    "CDI",
+    "CDD"
+  ],
+  "queries": [
+    "gouvernante EHPAD",
+    "gouvernant EHPAD",
+    "responsable hôtellerie EHPAD",
+    "responsable hôtelier EHPAD",
+    "responsable hébergement EHPAD",
+    "responsable services hôteliers",
+    "responsable administrative EHPAD",
+    "responsable administratif EHPAD",
+    "assistant direction EHPAD",
+    "assistante direction EHPAD",
+    "adjoint direction EHPAD",
+    "adjointe direction EHPAD",
+    "responsable administratif médico-social",
+    "responsable hôtellerie résidence senior",
+    "gouvernante résidence senior"
+  ],
+  "max_results_per_query": 100
+}
